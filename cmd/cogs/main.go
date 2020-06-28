4abedd6a57a2269b04dfd88e91dd2bb793f5d1c8
Mikhail Katychev <mkatych@gmail.com>
2020-06-28 16:09:10 -0500

    inital cli setup

--- cmd/cogs/main.go
+++ cmd/cogs/main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/bestowinc/cogs"
+	"github.com/docopt/docopt-go"
 )
 
+// var cmdMap = map[string]func(cog{
+//     "generate": generateCmd,
+// }
+
 func main() {
-	fmt.Println("Hello configs!")
+	usage := `Usage:
+  example generate <env> <cog-file>`
+
+	opts, _ := docopt.ParseArgs(usage, os.Args[1:], "0.1")
+	var conf struct {
+		Generate bool
+		Env      string
+		File     string `docopt:"<cog-file>"`
+	}
+
+	opts.Bind(&conf)
+	_ = fmt.Println
+	switch {
+	case conf.Generate:
+		if err := cogs.Generate(conf.Env, conf.File); err != nil {
+			panic(err)
+		}
+	}
 }
