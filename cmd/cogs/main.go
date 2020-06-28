de37688a1cc6c46336718e1f533d793a803c5e5a
Mikhail Katychev <mkatych@gmail.com>
2020-06-28 16:55:10 -0500

    comment cleanup

--- cmd/cogs/main.go
+++ cmd/cogs/main.go
@@ -8,10 +8,6 @@ import (
 	"github.com/docopt/docopt-go"
 )
 
-// var cmdMap = map[string]func(cog{
-//     "generate": generateCmd,
-// }
-
 func main() {
 	usage := `Usage:
   example generate <env> <cog-file>`
