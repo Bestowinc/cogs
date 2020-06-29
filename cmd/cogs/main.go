04e4ad780b1a0bc6b7b4b9bafa63537d7a0f4bde
Mikhail Katychev <mkatych@gmail.com>
2020-06-28 23:22:47 -0500

    cli revert

--- cmd/cogs/main.go
+++ cmd/cogs/main.go
@@ -2,33 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	"github.com/bestowinc/cogs"
-	"github.com/docopt/docopt-go"
 )
 
-// var cmdMap = map[string]func(cog{
-//     "generate": generateCmd,
-// }
-
 func main() {
-	usage := `Usage:
-  example generate <env> <cog-file>`
-
-	opts, _ := docopt.ParseArgs(usage, os.Args[1:], "0.1")
-	var conf struct {
-		Generate bool
-		Env      string
-		File     string `docopt:"<cog-file>"`
-	}
-
-	opts.Bind(&conf)
-	_ = fmt.Println
-	switch {
-	case conf.Generate:
-		if err := cogs.Generate(conf.Env, conf.File); err != nil {
-			panic(err)
-		}
-	}
+	fmt.Println("Hello configs!")
 }
