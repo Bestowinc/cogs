494f2c40184b309c7582dfb00bca21ac65da67ff
Mikhail Katychev <mkatych@gmail.com>
2020-06-28 19:46:24 -0500

    added basic cog example

--- generate.go
+++ generate.go
@@ -2,16 +2,17 @@ package cogs
 
 import (
 	"fmt"
-	"github.com/BurntSushi/toml"
+	"github.com/pelletier/go-toml"
 )
 
-type CogManifest struct {
+type Cog struct {
 	Name string
-	Envs map[string]Env
+	envs map[string]Env
 }
 
 type Env struct {
-	Cfg map[string]Cfg
+	Config map[string]Cfg
+	Enc    map[string]Cfg `toml:"enc"`
 }
 
 type Cfg struct {
@@ -21,10 +22,33 @@ type Cfg struct {
 }
 
 func Generate(env, cogFile string) error {
-	var manifest CogManifest
-	if _, err := toml.DecodeFile(cogFile, &manifest); err != nil {
+	// var manifest map[string]interface{}
+	var cog Cog
+	var ok bool
+
+	tree, err := toml.LoadFile(cogFile)
+	if err != nil {
 		return err
 	}
-	fmt.Println(manifest.Envs)
+
+	// grab manifest name
+	cog.Name, ok = tree.Get("name").(string)
+	if !ok || cog.Name == "" {
+		return fmt.Errorf("manifest.name string value must be present as a string")
+	}
+	// prep tree for unmarshaling
+	tree.Delete("name")
+
+	if err = tree.Unmarshal(&cog.envs); err != nil {
+		return err
+	}
+	fmt.Printf("%v\n", cog)
+	fmt.Println(cog.Name)
 	return nil
 }
+
+// Validate ensures the given toml.Tree does not contain reserved namespaces
+// func Validate(tree *toml.Tree) error {
+//     for _, k := tree.Keys()
+
+// }
