6b0637f2d96634881703ded7bf316c711fca4eb7
Mikhail Katychev <mkatych@gmail.com>
2020-11-21 16:41:36 -0600

    test inheritor in e2e

--- e2e.sh
+++ e2e.sh
@@ -3,5 +3,6 @@
 go run ./cmd/cogs gen docker basic.cog.toml
 go run ./cmd/cogs gen sops basic.cog.toml
 go run ./cmd/cogs gen kustomize basic.cog.toml
+go run ./cmd/cogs gen inheritor advanced.cog.toml
 go run ./cmd/cogs gen flat_json advanced.cog.toml
 go run ./cmd/cogs gen complex_json advanced.cog.toml
